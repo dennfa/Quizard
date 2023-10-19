3bc6e99b534aa3fedeae7a9fa36ae29d1abdc2f7
dennfa <dennis_fank@web.de>
2023-10-19 14:40:12 +0200

    add MultipleChoiceController and change html title

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,6 +1,9 @@
 export default function MainPage(){
     return(
         <>
+            <button>Take a Quiz</button>
+            <button>Create a Quiz</button>
+            <button>Update a Quiz</button>
         </>
     )
 }
