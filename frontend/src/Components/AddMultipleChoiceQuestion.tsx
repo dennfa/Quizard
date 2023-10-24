60decfe5d7b24435173c0fa1dbeffe8e4cae5972
dennfa <dennis_fank@web.de>
2023-10-24 20:46:07 +0200

    add MultipleChoiceComponent to CreateQuiz

--- frontend/src/Components/AddMultipleChoiceQuestion.tsx
+++ frontend/src/Components/AddMultipleChoiceQuestion.tsx
@@ -1,25 +1,48 @@
+import {MultipleChoiceQuestion} from "../Models/MultipleChoiceQuestion.tsx";
+import {useEffect, useState} from "react";
 
-export default function AddMultipleChoiceQuestion(){
+export type props = {
+    multipleChoiceQuestion: MultipleChoiceQuestion,
+    index: number,
+    myCallBack: (questionData: MultipleChoiceQuestion, index: number) => void,
+}
 
-    return(
+export default function AddMultipleChoiceQuestion(props: props) {
+
+    const [question, setQuestion] = useState<string>("")
+    const [falseAnswer, setFalseAnswer] = useState<string>("")
+    const [trueAnswer, setTrueAnswer] = useState<string>("")
+
+    useEffect(() => {
+        props.myCallBack({question, falseAnswer, trueAnswer}, props.index)
+    }, [question, falseAnswer, trueAnswer])
+
+
+    return (
         <div>
             <label htmlFor="question">Question:</label>
-        <input
-            id="question"
-            name="question"
-            type="text"
+            <input
+                id="question"
+                name="question"
+                type="text"
+                value={props.multipleChoiceQuestion.question}
+                onChange={event => setQuestion(event.target.value)}
             />
             <label htmlFor="falseAnswer">False Answer:</label>
             <input
                 id="falseAnswer"
                 name="falseAnswer"
                 type="text"
+                value={props.multipleChoiceQuestion.falseAnswer}
+                onChange={event => setFalseAnswer(event.target.value)}
             />
             <label htmlFor="trueAnswer">True Answer:</label>
             <input
                 id="trueAnswer"
                 name="trueAnswer"
                 type="text"
+                value={props.multipleChoiceQuestion.trueAnswer}
+                onChange={event => setTrueAnswer(event.target.value)}
             />
         </div>
     )
--- frontend/src/Pages/CreateQuiz.tsx
+++ frontend/src/Pages/CreateQuiz.tsx
@@ -1,34 +1,53 @@
-import {JSX, useState} from "react";
+import {useState} from "react";
+import {MultipleChoiceQuestion} from "../Models/MultipleChoiceQuestion.tsx";
 import AddMultipleChoiceQuestion from "../Components/AddMultipleChoiceQuestion.tsx";
 
 export default function CreateQuiz() {
 
-    const [questions, setQuestions] = useState<JSX.Element[]>([])
+    const [questions, setQuestions] = useState<MultipleChoiceQuestion[]>([])
 
     function handleAddQuestion() {
-        const newQuestion = <AddMultipleChoiceQuestion key={questions.length}/>;
+        const newQuestion: MultipleChoiceQuestion = {
+            question: "",
+            falseAnswer: "",
+            trueAnswer: "",
+        }
         setQuestions([...questions, newQuestion]);
     }
 
-    function saveQuiz(){
+    function myCallBackFunction(questionData:MultipleChoiceQuestion, index:number){
+        console.log(questionData)
+        const questions2 = questions.slice()
+        questions2[index] = questionData
+        setQuestions(questions2)
+    }
+
+    function saveQuiz() {
         console.log("Hello")
- }
+    }
 
     return (
         <div className="CreateQuiz">
-            <div className="QuizName">
-                <label htmlFor="quizName">Quiz Name:</label>
-                <input
-                    type="text"
-                    id="quizName"
-                    name="quizName"
-                    placeholder="Enter the name of your quiz here"
-                />
-            </div>
-            <p>Current number of questions: {questions.length}</p>
-            {questions}
-            <button onClick={handleAddQuestion}>Add Question</button>
-            <button onClick={saveQuiz}>Save Quiz</button>
+            <form>
+                <div className="QuizName">
+                    <label htmlFor="quizName">Quiz Name:</label>
+                    <input
+                        type="text"
+                        id="quizName"
+                        name="quizName"
+                        placeholder="Enter the name of your quiz here"
+                    />
+                </div>
+                <p>Current number of questions: {questions.length}</p>
+                {questions.map((question: MultipleChoiceQuestion, index: number) => <AddMultipleChoiceQuestion
+                    key={index}
+                    index={index}
+                    multipleChoiceQuestion={question}
+                    myCallBack={myCallBackFunction}
+                    />)}
+                <button type="button" onClick={handleAddQuestion}>Add Question</button>
+                <button onClick={saveQuiz}>Save Quiz</button>
+            </form>
         </div>
     )
 }
