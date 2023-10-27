591a51fe5facb78172d013f60813bb31f8cff033
dennfa <dennis_fank@web.de>
2023-10-27 15:57:11 +0200

    Add Route Path TakeQuiz

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import MainPage from "./Pages/MainPage.tsx";
 import CreateQuiz from "./Pages/CreateQuiz.tsx";
+import TakeQuiz from "./Pages/TakeQuiz.tsx";
 
 export default function App() {
 
@@ -9,6 +10,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<MainPage/>}/>
       <Route path="/create" element={<CreateQuiz/>}/>
+      <Route path="/take" element={<TakeQuiz/>}/>
     </Routes>
   )
 }
