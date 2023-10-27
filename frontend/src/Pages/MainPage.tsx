c14e074156231c75ab742117603f45b8df73b1ec
dennfa <dennis_fank@web.de>
2023-10-27 17:49:49 +0200

    add TakeQuiz route

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -4,14 +4,11 @@ export default function MainPage() {
 
     const navigate = useNavigate();
 
-    const handleCreateQuizClick = ()=>{
-        navigate("/create");
-    }
 
     return (
             <div className="MainPage">
-                <button>Take a Quiz</button>
-                <button onClick={handleCreateQuizClick}>Create a Quiz</button>
+                <button onClick={()=>navigate("/take")}>Take a Quiz</button>
+                <button onClick={()=>navigate("/create")}>Create a Quiz</button>
                 <button>Update a Quiz</button>
                 </div>
     )
