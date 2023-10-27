31c11ec0776c7ed82b3e6f25fab49c143755118f
dennfa <dennis_fank@web.de>
2023-10-27 15:55:51 +0200

    Add Lombok Generated to ignore BackendApplication at test coverage

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,11 +1,13 @@
 package com.example.backend;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
