55378e234ca8f4adf26c77489e57f0d45ec7a64c
dennfa <dennis_fank@web.de>
2023-11-03 07:59:27 +0100

    add TakeQuiz route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import CreateQuiz from "./Pages/CreateQuiz.tsx";
 import TakeQuizList from "./Pages/TakeQuizList.tsx";
 import UpdateQuizList from "./Pages/UpdateQuizList.tsx";
 import UpdateQuiz from "./Pages/UpdateQuiz.tsx";
+import TakeQuiz from "./Pages/TakeQuiz.tsx";
 
 export default function App() {
 
@@ -13,6 +14,7 @@ export default function App() {
       <Route path="/" element={<MainPage/>}/>
       <Route path="/create" element={<CreateQuiz/>}/>
       <Route path="/take" element={<TakeQuizList/>}/>
+      <Route path="/take/:id" element={<TakeQuiz/>}/>
       <Route path="/update" element={<UpdateQuizList/>}/>
       <Route path="/update/:id" element={<UpdateQuiz/>}/>
       <Route path="/*" element={<Navigate to={"/"}/>}/>
