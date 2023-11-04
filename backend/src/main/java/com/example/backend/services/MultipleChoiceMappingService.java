e21e51666119de9be5df415fe8c26effd27c8b58
dennfa <dennis_fank@web.de>
2023-11-04 15:06:48 +0100

    add mapMultipleChoiceQuizToPlayMcq

---
+++ backend/src/main/java/com/example/backend/services/MultipleChoiceMappingService.java
@@ -0,0 +1,36 @@
+package com.example.backend.services;
+
+import com.example.backend.models.MultipleChoiceQuiz;
+import com.example.backend.models.play.PlayMultipleChoiceQuestion;
+import com.example.backend.models.play.PlayMultipleChoiceQuiz;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+@RequiredArgsConstructor
+public class MultipleChoiceMappingService {
+
+    public PlayMultipleChoiceQuiz mapMultipleChoiceQuizToPlayMcq(MultipleChoiceQuiz mcq) {
+
+        List<PlayMultipleChoiceQuestion> pmcqList = mcq.multipleChoiceQuestions()
+                .stream().map(mcqQuestion ->
+                        PlayMultipleChoiceQuestion.builder()
+                                .question(mcqQuestion.question())
+                                .answers(Set.of(mcqQuestion.falseAnswer(), mcqQuestion.trueAnswer()))
+                                .build()
+                ).toList();
+
+
+        return PlayMultipleChoiceQuiz.builder()
+                .id(mcq.id())
+                .name(mcq.id())
+                .description(mcq.description())
+                .numberOfQuestions(mcq.numberOfQuestions())
+                .playMultipleChoiceQuestions(pmcqList)
+                .build();
+    }
+}
