4eb2b4ab5125ccf2010ce244ae0abdb9111c22f6
dennfa <dennis_fank@web.de>
2023-11-12 15:17:22 +0100

    introduce utility class to reduce duplicated code

--- frontend/src/Pages/CreateQuiz.tsx
+++ frontend/src/Pages/CreateQuiz.tsx
@@ -5,6 +5,7 @@ import {MultipleChoiceQuiz} from "../Models/MultipleChoiceQuiz.tsx";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import DeleteIcon from "../Assets/trash.svg"
+import {multipleChoicePropertyAction} from "../Utility/Utility.tsx";
 
 export default function CreateQuiz() {
 
@@ -28,23 +29,7 @@ export default function CreateQuiz() {
 
     function myCallBackFunction(userInput: string, multipleChoiceProperty: string, index: number) {
         const updatedQuestions = questions.slice()
-        switch (multipleChoiceProperty) {
-            case "question":
-                updatedQuestions[index].question = userInput
-                break
-            case "falseAnswer1":
-                updatedQuestions[index].falseAnswers[0] = userInput
-                break
-            case "falseAnswer2":
-                updatedQuestions[index].falseAnswers[1] = userInput
-                break
-            case "falseAnswer3":
-                updatedQuestions[index].falseAnswers[2] = userInput
-                break
-            case "trueAnswer":
-                updatedQuestions[index].trueAnswer = userInput
-                break
-        }
+        multipleChoicePropertyAction(userInput,multipleChoiceProperty,index,updatedQuestions)
         setQuestions(updatedQuestions)
     }
 
--- frontend/src/Pages/UpdateQuiz.tsx
+++ frontend/src/Pages/UpdateQuiz.tsx
@@ -5,6 +5,7 @@ import {MultipleChoiceQuiz} from "../Models/MultipleChoiceQuiz.tsx";
 import {MultipleChoiceQuestion} from "../Models/MultipleChoiceQuestion.tsx";
 import AddMultipleChoiceQuestion from "../Components/AddMultipleChoiceQuestion.tsx";
 import DeleteIcon from "../Assets/trash.svg";
+import {multipleChoicePropertyAction} from "../Utility/Utility.tsx";
 
 export default function UpdateQuiz() {
 
@@ -47,23 +48,7 @@ export default function UpdateQuiz() {
 
     function myCallBackFunction(userInput: string, multipleChoiceProperty: string, index: number) {
         const updatedQuestions = multipleChoiceQuiz.multipleChoiceQuestions.slice()
-        switch (multipleChoiceProperty) {
-            case "question":
-                updatedQuestions[index].question = userInput
-                break
-            case "falseAnswer1":
-                updatedQuestions[index].falseAnswers[0] = userInput
-                break
-            case "falseAnswer2":
-                updatedQuestions[index].falseAnswers[1] = userInput
-                break
-            case "falseAnswer3":
-                updatedQuestions[index].falseAnswers[2] = userInput
-                break
-            case "trueAnswer":
-                updatedQuestions[index].trueAnswer = userInput
-                break
-        }
+        multipleChoicePropertyAction(userInput,multipleChoiceProperty,index,updatedQuestions)
         setMultipleChoiceQuiz({...multipleChoiceQuiz, multipleChoiceQuestions: updatedQuestions})
     }
 
